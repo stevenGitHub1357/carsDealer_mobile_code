ebbba51a7451c4cbda54d3eaa6f1046f4df59cd3
stevenGitHub1357 <stevenratovoson@gmail.com>
2024-03-05 10:17:36 +0300

    Update url web service

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'https://carsdealerwebservice-production.up.railway.app', // Remplacez par l'URL de votre backend
+  baseURL: 'https://carsdealerwebservice-production-1aa2.up.railway.app/', // Remplacez par l'URL de votre backend
   headers: {
     'Content-Type': 'application/json',
   },
